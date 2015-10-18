46b82074ba0995f6ba6f4a4a5fc5240ec05e622e
Leonardo Soroeta <lsoroeta@gmail.com>
2015-10-18 05:36:12 -0300

    commit!!!

---
+++ src/GameStack.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class GameStack {
+	
+	List<GameStackAction> gameStack;
+	
+	public GameStack() {
+		gameStack = new LinkedList<GameStackAction>();    //no se si linked list o que tiene q ser
+	}
+	
+	//recursiva va pidiendo mas spells o abilities a los jugadores
+	public void stackAction(GameStackAction gameStackAction) {
+		gameStack.add(gameStackAction);
+		//if(new spell or action)
+		//		stackAction(new stackableAction);
+		//switchPriority();
+		//if(new spell or action)
+		//				stackAction(new stackableAction);
+		//switchPriority();
+		gameStackAction.resolve();
+		gameStack.remove(gameStackAction);
+	}
+	
+}
