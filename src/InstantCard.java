7ede111d13d51fbfd72d483efe2567441d95d26d
marcos <marcos@marcos-MS-7529>
2015-10-18 12:41:46 -0300

    clases de cartas falta terminar metodo playCard!!!

--- src/InstantCard.java
+++ src/InstantCard.java
@@ -2,20 +2,21 @@
 public class InstantCard extends SpellCard {
 
 	
-	public InstantCard(String nameCard, String color, int colorMana, int colorlessMana, Ability ability) {
-		super(nameCard, color, colorMana, colorlessMana, ability);
+	public InstantCard(String nameCard, String typeCard, String color, int colorMana, int colorlessMana, Ability ability) {
+		super(nameCard, typeCard, color, colorMana, colorlessMana, ability);
 	}
 	
 
 	public Ability playCard(ManaPool manaPool) {
 		
-		if(manaPool.getMana(this.getColor()) >= this.getColorMana() && manaPool.getMana("Colorless") >= this.getColorlessMana()){
+		//if(manaPool.getMana(this.getColor()) >= this.getColorMana() && manaPool.getMana("Colorless") >= this.getColorlessMana()){
 			manaPool.decreaseMana(this.getColor(),  this.getColorMana());
 			manaPool.decreaseMana("Colorless", this.getColorlessMana());
 			return this.getAbility();
-		}else{
+		//}else{
 			//tira Excepsion!!!
-		}
+		//}
+
 	}
 	
 	
