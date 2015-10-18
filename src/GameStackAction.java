7db6d2151a6135a022fd0bad6d355dcd58ec345b
Leonardo Soroeta <lsoroeta@gmail.com>
2015-10-18 05:36:29 -0300

    commit!!!

---
+++ src/GameStackAction.java
@@ -0,0 +1,8 @@
+
+public interface GameStackAction {
+	
+	public void toStack(); // capaz al pedo este metodo, se podria llamar directamente stackAtion del GameStack. por ahora queda lindo
+	
+	public void resolve(); // pasa del stack a entrar en juego o hacer lo q tiene q hacer
+	
+}
