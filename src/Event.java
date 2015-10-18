e8cad1989fbcdc78e303bb92c01604c0ef8488ad
Leonardo Soroeta <lsoroeta@gmail.com>
2015-10-18 04:34:38 -0300

    commit!!!

--- src/Event.java
+++ src/Event.java
@@ -1,4 +1,14 @@
 
 public class Event {
+	
+	/* hay que tener otros, pero este es el constructor del evento mas simple, ejemplo:
+	 * 		new Event("upkeep_phase", activePlayer());
+	 * pero tambien habria que tener Events por ejemplo del estilo:
+	 * 		new Event("card_tapped", laCarta, porTalJugador);
+	 * y luego se lo manda al event handler para que se fije si tiene que pasar algo
+	 */
+	public Event(String event, Player player) {
+		
+	}
 
 }
