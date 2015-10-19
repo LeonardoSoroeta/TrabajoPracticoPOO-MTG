aeaa5c9c9a4d7028bb6d4811f7d01b5043ddcdbd
Leonardo Soroeta <lsoroeta@gmail.com>
2015-10-19 01:33:48 -0300

    commit!!!

---
+++ src/backend/DamageCounter.java
@@ -0,0 +1,18 @@
+package backend;
+
+/*
+ * damage va al stack. asi el otro tiene chance de prevenirlo 
+ * menos el damage de combate que se aplica de una. ba esa es la regla nueva
+ * de magic y pinta mas facil pero podemos ponerlo en el stack tambien
+ */
+
+public class DamageCounter implements GameStackAction {
+	
+	public DamageCounter() {
+		
+	}
+	
+	public void resolve() {
+		
+	}
+}
