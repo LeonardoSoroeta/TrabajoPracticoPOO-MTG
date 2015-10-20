9ce90b5b1c0ac19491da286dec9c33ff57d4397f
Leonardo Soroeta <lsoroeta@gmail.com>
2015-10-20 14:57:28 -0300

    asdfasdf

--- src/backend/Event.java
+++ src/backend/Event.java
@@ -2,14 +2,36 @@ package backend;
 
 public class Event {
 	
-	/* hay que tener otros, pero este es el constructor del evento mas simple, ejemplo:
-	 * 		new Event("upkeep_phase", activePlayer());
+	private String description;
+	//private Object target;
+	//private Object source;
+	//private Integer turn;
+	
+	/* hay que tener otros, pero este es un tipo de constructor, ejemplo:
+	 * 		new Event("upkeep_phase");
 	 * pero tambien habria que tener Events por ejemplo del estilo:
 	 * 		new Event("card_tapped", laCarta, porTalJugador);
 	 * y luego se lo manda al event handler para que se fije si tiene que pasar algo
 	 */
-	public Event(String event, Player player) {
+	public Event(String description) {
+		this.description = description;
+	}
+	
+	/* seria como un equals para saber si un evento satisface a otro. por ejemplo todo evento deberia 
+	 * satisfacer el evento generico new Event("all_events") que gatilla una habilidad cuando
+	 * sucede cualquier evento. o quizas el evento new Event("permanent_into_play", byPlayer);
+	 * deberia satisfacer el evento mas general new Event("permanent_into_play") que necesita gatillar
+	 * una habilidad cuando algun un permanent entra en juego sin importar el jugador.
+	 */
+	public Boolean satisfies(Event event) {
 		
+		if(event.getDescription().equals("all_events"))
+			return true;
+		
+	}
+	
+	public String getDescription() {
+		return description;
 	}
 
 }
