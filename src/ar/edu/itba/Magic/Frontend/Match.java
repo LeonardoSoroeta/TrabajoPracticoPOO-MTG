fd786ce6e18c6c8b7dee229aa29f67caf39c543a
Santiago <burgossantiago@hotmail.com>
2015-10-21 17:53:59 -0300

    >=|

---
+++ src/ar/edu/itba/Magic/Frontend/Match.java
@@ -0,0 +1,46 @@
+package ar.edu.itba.Magic.Frontend;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+//slick
+public class Match extends BasicGameState {
+
+	
+public Match( int state){
+		
+	}
+
+
+	
+	public void init(GameContainer arg0, StateBasedGame arg1)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 2;
+	}
+
+}
