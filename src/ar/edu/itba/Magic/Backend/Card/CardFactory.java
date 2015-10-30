ca5235a62f77688f5aedf630921c92436d2bd4e0
Leonardo Soroeta <lsoroeta@gmail.com>
2015-10-30 20:32:57 -0300

    card factory

--- src/ar/edu/itba/Magic/Backend/Card/CardFactory.java
+++ src/ar/edu/itba/Magic/Backend/Card/CardFactory.java
@@ -1,16 +1,6 @@
-package ar.edu.itba.Magic.Backend.Card;
+package ar.edu.itba.Magic.Backend;
 //import ar.edu.itba.Magic.Frontend.Match;
 import java.util.*;
-
-import ar.edu.itba.Magic.Backend.ActivatedPermanentAbility;
-import ar.edu.itba.Magic.Backend.AutomaticPermanentAbility;
-import ar.edu.itba.Magic.Backend.Creature;
-import ar.edu.itba.Magic.Backend.GameEvent;
-import ar.edu.itba.Magic.Backend.GameEventHandler;
-import ar.edu.itba.Magic.Backend.Match;
-import ar.edu.itba.Magic.Backend.Permanent;
-import ar.edu.itba.Magic.Backend.Player;
-import ar.edu.itba.Magic.Backend.SpellAbility;
 
 public class CardFactory {
 	
@@ -27,6 +17,11 @@ public class CardFactory {
         return instance;
 	}
 	
+	/**
+	 * Creates a list of default attributes contained by creatures.
+	 * 
+	 * @return Returns a list of default attributes contained by creatues.
+	 */
 	public List<String> getDefaultCreatureAttributes() {
 		List<String> attributes = new ArrayList<String>();
 		
@@ -39,6 +34,11 @@ public class CardFactory {
 		return attributes;
 	}
 	
+	/**
+	 * Creates a list of default attributes contained by enchantments.
+	 * 
+	 * @return Returns a list of default attributes contained by enchantments.
+	 */
 	public List<String> getDefaultEnchantmentAttributes() {
 		List<String> attributes = new ArrayList<String>();
 		
@@ -51,22 +51,89 @@ public class CardFactory {
 		
 		switch(cardName) {
 		
+			case "Bad Moon":
+				attributes = getDefaultEnchantmentAttributes();
+				return new EnchantmentCard("Bad Moon", "enchantment", "black", attributes, 1, 1, 
+						new AutomaticPermanentAbility() {
+					
+							/**
+							 * Adds Bad Moon's automatic ability to the GameEventHandler.
+							 */
+							@Override 
+							public void executeOnIntroduction() {
+								gameEventHandler.add(this);
+							}
+							
+							@Override
+							public void executeOnEvent(GameEvent gameEvent) {
+								List<Permanent> permanents = new LinkedList<Permanent>();
+								permanents.addAll(match.getPlayer1().getPermanentsInPlay());
+								permanents.addAll(match.getPlayer2().getPermanentsInPlay());
+								for(Permanent permanent : permanents) {
+									if(permanent instanceof Creature) {
+										if(permanent.getColor().equals("black")) {
+											
+										}
+											
+									}
+								}
+								
+							}				
+				});
+
 			case "Bog Imp":
 				attributes = getDefaultCreatureAttributes();
 				attributes.add("flying");
 				return new CreatureCard("Bog Imp", "creature", "black", attributes, 1, 1, 1, 1);
 				
+				
 			case "Flood":
 				attributes = getDefaultEnchantmentAttributes();
 				return new EnchantmentCard("Flood", "enchantment", "blue", attributes, 1, 0, 
 						new ActivatedPermanentAbility() {
-
+							
+							/**
+							 * Pay 2 blue mana to tap a target creature without flying.
+							 */
+							@Override
 							public void executeOnActivation() {
+								//TODO
 								//pay mana cost
 								//select target creature without flying
 								//creature.tap();
 							}
 				});
+				
+			case "Lord of the Pit":
+				attributes = getDefaultCreatureAttributes();
+				attributes.add("trample");
+				attributes.add("flying");
+				return new CreatureCard("Lord Of The Pit", "creature", "black", attributes, 3, 4, 7, 7,
+						new AutomaticPermanentAbility() {
+							
+							/**
+							 * Adds Lord of the Pit's automatic ability to the GameEventHandler.
+							 */
+							@Override
+							public void executeOnIntroduction() {
+								gameEventHandler.add(this);
+							}
+							
+							/**
+							 * Activates on Lord of the Pit's controller's upkeep. Requires the player to sacrifice
+							 * a creature or Lord of the Pit deals him 7 damage.
+							 */
+							@Override
+							public void executeOnEvent(GameEvent gameEvent) {
+								if(gameEvent.getDescriptor().equals("upkeep_step"))
+									if(gameEvent.getObject1().equals((this.getSource()).getController())) {
+										//TODO 
+										//select a creature, destroy it
+										//otherwise, suffer 7 damage
+									}
+							}
+				});
+				
 				
 			case "Nightmare":
 				attributes = getDefaultCreatureAttributes();
@@ -75,30 +142,51 @@ public class CardFactory {
 						new AutomaticPermanentAbility() {
 							
 							/**
-							 * Adds Nightmare's automatic ability to the gameEventHandler.
+							 * Adds Nightmare's automatic ability to the GameEventHandler. Executes a generic
+							 * game event to get Nightmare's attack and defense started.
 							 */
+							@Override
 							public void executeOnIntroduction() {
 								gameEventHandler.add(this);
 								gameEventHandler.notifyGameEvent(new GameEvent("generic_event"));
 							}
 							
 							/**
-							 * Executes on every game event. Sets Nightmare's attack and defense equal to
+							 * Activates on every game event. Sets Nightmare's attack and defense equal to
 							 * the ammount of Swamps it's controller has in play.
 							 */
+							@Override
 							public void executeOnEvent(GameEvent gameEvent) {
 								Integer swamps = 0;
-								Player controller = ((Permanent)this.getSource()).getController();
+								Player controller = (this.getSource()).getController();
 								List<Permanent> permanents = new LinkedList<Permanent>();
 								permanents.addAll(controller.getPermanentsInPlay());
 								for(Permanent permanent : permanents) {
 									if(permanent.getName().equals("Swamp")) 
 										swamps++;
-								((Creature)this.getSource()).setAttack(swamps);
-								((Creature)this.getSource()).setDefense(swamps);
 								}
-								
+								((Creature)this.getSource()).setAttack(swamps);
+								((Creature)this.getSource()).setDefense(swamps);					
 							}
+				});
+				
+			case "Royal Assassin":
+				attributes = getDefaultCreatureAttributes();
+				return new CreatureCard("Royal Assassin", "creature", "black", attributes, 2, 1, 1, 1,
+						new ActivatedPermanentAbility() {
+							
+							/**
+							 * Taps royal assassin to destroy a target tapped creature.
+							 */
+							@Override
+							public void executeOnActivation() {
+								if(this.getSource().isTapped())
+									System.out.println("cannot tap"); //TODO cambiar esto
+								else {
+									//TODO select target tapped creature
+									//target.destroy();
+								}								
+							}		
 				});
 
 			case "Terror":
@@ -107,17 +195,25 @@ public class CardFactory {
 					
 							private Creature target;
 							
+							/**
+							 * Player must select a target non black non artifact creature.
+							 */
+							@Override
 							public boolean satisfyCastingRequirements() {
+								//TODO
 								//seleccionar un target
 									//return true
 								//else
 								return false;
 							}
-
+							
+							@Override
 							public void sendToStack() {
+								//TODO
 								//gamestack.add(this)
 							}
-					
+							
+							@Override
 							public void resolveInStack() {
 								target.destroy();
 							}
