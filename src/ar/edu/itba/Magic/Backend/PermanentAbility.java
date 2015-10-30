0db4225caa0779ebba95322ad6baffbe8bf2fb20
Leonardo Soroeta <lsoroeta@gmail.com>
2015-10-30 19:37:41 -0300

    override de setSource y getSource para que se trate de Permanent y no Object y no haya que estar casteando a Permanent todo el tiempo

--- src/ar/edu/itba/Magic/Backend/PermanentAbility.java
+++ src/ar/edu/itba/Magic/Backend/PermanentAbility.java
@@ -5,7 +5,17 @@ package ar.edu.itba.Magic.Backend;
  * it's ability's executeOnIntroduction method must be executed.
  */
 public abstract class PermanentAbility extends Ability {
+	
+	private Permanent source;
+	
+	public Permanent getSource() {
+		return source;
+	}
 
+	public void setSource(Permanent source) {
+		this.source = source;
+	}
+	
 	/**
 	 * Must execute this method every time the ability's Permanent source enters play.
 	 */
