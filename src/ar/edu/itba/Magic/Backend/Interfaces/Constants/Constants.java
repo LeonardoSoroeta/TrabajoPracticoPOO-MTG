5d3e079f4ff1d9b9a30019325749a8358cb73500
Martin Capparelli <martinfedericocapparelli@gmail.com>
2015-11-02 18:25:06 -0300

    START_HEALTH constant.

--- src/ar/edu/itba/Magic/Backend/Interfaces/Constants/Constants.java
+++ src/ar/edu/itba/Magic/Backend/Interfaces/Constants/Constants.java
@@ -5,4 +5,5 @@ public interface Constants {
 	public static final int VIRTUAL_WIDTH = 800;
 	public static final int VIRTUAL_HEIGHT = 600;
 	public static final float ASPECT_RATIO = (float)VIRTUAL_WIDTH/(float)VIRTUAL_HEIGHT;
+	public static final Integer START_HEALTH = 20;
 }
