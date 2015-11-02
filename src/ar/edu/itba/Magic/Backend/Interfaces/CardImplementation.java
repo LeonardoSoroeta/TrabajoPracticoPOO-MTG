7b7df6ccd4ea44898c44bc6f7548b56ce12b1224
Martin Capparelli <martinfedericocapparelli@gmail.com>
2015-11-02 19:33:32 -0300

    CardImplementation listo.

--- src/ar/edu/itba/Magic/Backend/Interfaces/CardImplementation.java
+++ src/ar/edu/itba/Magic/Backend/Interfaces/CardImplementation.java
@@ -1,8 +1,13 @@
 package ar.edu.itba.Magic.Backend.Interfaces;
 
+
+import javax.smartcardio.Card;
+
 /**
  * Created by Martin on 02/11/2015.
  */
-public interface CardImplementation {
-    
+public interface CardImplementation<T extends Card> {
+
+    public T CardImpllementation();
+
 }
