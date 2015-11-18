e0e0429e0d629dd2bc4857ad7050dda6758ce87c
Santiago <burgossantiago@hotmail.com>
2015-11-18 00:33:17 -0300

    2.0

--- src/ar/edu/itba/Magic/Frontend/NewMatchState.java
+++ src/ar/edu/itba/Magic/Frontend/NewMatchState.java
@@ -246,44 +246,50 @@ public class NewMatchState extends BasicGameState {
 			
 		
 		if( match.getMatchState().equals(MatchState.AWAITING_ABILITY_TARGET_SELECTION) || match.getMatchState().equals(MatchState.AWAITING_CASTING_TARGET_SELECTION)){
-			
+			boolean selected = false;
+					
 			for(Permanent permanent: player1.getPermanentsInPlay()){	
 				if( decklistpl1.getTinyCard(permanent).mouseLClicked(input)){
 					match.returnSelectedTarget(permanent);
 					match.update();
-					break;
+					selected = true;
+					
 					}
 			}
 			
+			if(selected == false){
 			for(Permanent permanent: player2.getPermanentsInPlay()){
 			
 				if( decklistpl2.getTinyCard(permanent).mouseLClicked(input)){
 					match.returnSelectedTarget(permanent);
 					match.update();
-					break;
+					selected = true;
 				}
 			}
-		
+			}
 			
+			if(selected == false){
 				for(Object object: match.getGameStack().getPlayer1STackObjects()){
 				
 					if( decklistpl1.getTinyCard((Permanent)object).mouseLClicked(input)){
 						match.returnSelectedTarget(object);
 						match.update();
-						break;
+						selected = true;
 					}
 				}
+			}
 			
 			
-			
+			if(selected == false){
 				for(Object object: match.getGameStack().getPlayer2StackObjects()){
 				
 					if( decklistpl2.getTinyCard((Permanent)object).mouseLClicked(input)){
 						match.returnSelectedTarget(object);
 						match.update();
-						break;
+						selected = true;
 					}
 				}
+			}
 				
 				
 			if (cancelbutton.mouseLClicked(input)){
