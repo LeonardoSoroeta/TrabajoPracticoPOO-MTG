b7db63e502a1c240daf4322f7de1dee62a406da6
Martin Capparelli <martinfedericocapparelli@gmail.com>
2015-11-19 17:28:55 -0300

    PlayerTestListo

--- src/ar/edu/itba/Magic/Backend/Test/PlayerTest.java
+++ src/ar/edu/itba/Magic/Backend/Test/PlayerTest.java
@@ -1,11 +1,14 @@
 package ar.edu.itba.Magic.Backend.Test;
 
 import ar.edu.itba.Magic.Backend.Deck;
+import ar.edu.itba.Magic.Backend.Enums.CardType;
+import ar.edu.itba.Magic.Backend.ManaPool;
+import ar.edu.itba.Magic.Backend.Permanents.Permanent;
 import ar.edu.itba.Magic.Backend.Player;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Created by Martin on 19/11/2015.
@@ -13,10 +16,15 @@ import static org.junit.Assert.assertEquals;
 public class PlayerTest {
 
     private Player player;
+    private Player player2;
+    private Deck deck;
+    private Permanent permanent;
 
     @Before
     public void initializePlayer(){
         player = new Player(new Deck());
+        deck.addCard(CardType.BOG_IMP.createCardOfThisType());
+        player2 = new Player(deck);
     }
 
     @Test
@@ -45,6 +53,15 @@ public class PlayerTest {
 
     @Test
     public void getManaPoolTest(){
+        ManaPool manapool = player.getManaPool();
+        assertEquals(player.getManaPool(),manapool);
+    }
 
+    @Test
+    public void lostTest(){
+        player.setHealth(0);
+        assertTrue(player.lost());
+        player.setHealth(10);
+        assertFalse(player.lost());
     }
 }
