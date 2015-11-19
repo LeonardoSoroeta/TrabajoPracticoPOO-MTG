4b1e8c218bca0e2e0b3418344da1f4e53ee2a917
Leonardo Soroeta <lsoroeta@gmail.com>
2015-11-19 18:45:43 -0300

    asdfasdf

--- src/ar/edu/itba/Magic/Backend/Interfaces/Spell.java
+++ src/ar/edu/itba/Magic/Backend/Interfaces/Spell.java
@@ -1,14 +1,11 @@
 package ar.edu.itba.Magic.Backend.Interfaces;
 
+/** Implemented by spells that go on the spell stack */
 public interface Spell {
 
-	/**
-	 * Sends the hability or permanent to the stack.
-	 */
+	public void sendToStack();
 
-	public void sendToStack(); // manda al stack y hace lo que tenga que hacer antes (ejemplo determinar targets)
-
-	public void resolveInStack(); // pasa del stack a entrar en juego o hacer lo q tiene q hacer
+	public void resolveInStack();
 	
 	public void counterSpell();
 
